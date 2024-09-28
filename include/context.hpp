13efa545a788ae61aa3e32b96954185ceafce463
Mohamed Rezk <mohrizq895@gmail.com>
2024-09-28 19:56:51 +0300

    Refactor app by implementing a context class to avoid global data

---
+++ include/context.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <glm/vec4.hpp>
+#include <iostream>
+
+#include "glad.h"
+
+class Context {
+  private:
+    SDL_DisplayMode dm;
+
+  public:
+    int width, height;
+    bool is_quit = false;
+
+    SDL_Window *window = nullptr;
+    SDL_GLContext gl_context = nullptr;
+
+    Context() = default;
+
+    glm::vec4 get_viewport() { return glm::vec4(0.0f, 0.0f, width, height); }
+
+    void initialize_sdl() {
+        assert(SDL_Init(SDL_INIT_VIDEO) >= 0 && "Cannot initialize SDL");
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                            SDL_GL_CONTEXT_PROFILE_CORE);
+    }
+
+    void initialize_opengl() {
+        gl_context = SDL_GL_CreateContext(window);
+        assert(gl_context != nullptr && "Cannot create opengl context");
+        assert(gladLoadGLLoader(SDL_GL_GetProcAddress) &&
+               "Cannot initialize glad");
+    }
+
+    void create_window(int _width, int _height) {
+        width = _width;
+        height = _height;
+        initialize_sdl();
+        window = SDL_CreateWindow("mesher", SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, width, height,
+                                  SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+        assert(window != nullptr && "Cannot create window");
+        SDL_SetWindowFullscreen(window, SDL_FALSE);
+        initialize_opengl();
+    }
+
+    void update_window() {
+        SDL_GetCurrentDisplayMode(0, &dm);
+        width = dm.w;
+        height = dm.h;
+    }
+
+    ~Context() {
+        std::cout << "I am leaving" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+};
--- src/app.cpp
+++ src/app.cpp
@@ -1,98 +1,64 @@
+#include <SDL2/SDL_mouse.h>
 #include <SDL2/SDL_timer.h>
+#include <filesystem>
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <thread>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
-#include <SDL2/SDL_video.h>
 #include <glm/detail/type_vec.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "../include/bvh.hpp"
 #include "../include/camera.hpp"
+#include "../include/context.hpp"
 #include "../include/glad.h"
 #include "../include/mesh.hpp"
 #include "../include/shader.hpp"
 
 namespace fs = std::filesystem;
 
-const fs::path this_filepath =  __FILE__;
+const fs::path this_filepath = __FILE__;
 const fs::path ROOT = this_filepath.parent_path().parent_path();
 
-int WIDTH = 1200;
-int HEIGHT = 900;
-bool QUIT = false;
-
-SDL_Window *WINDOW = nullptr;
-SDL_DisplayMode DM;
-SDL_GLContext GL_CONTEXT = nullptr;
+Context ctx;
 
 Shader shader;
-Mesh mesh;
-Camera camera(
-    glm::vec3(1.0f, 2.0f, 2.0f), // pos of camera
-    glm::vec3(0.0f, 0.0f, 0.0f)  // where camera is looking
+Mesh mesh, mesh_box, triangle;
+Camera camera(glm::vec3(1.0f, 2.0f, 2.0f), // pos of camera
+              glm::vec3(0.0f, 0.0f, 0.0f)  // where camera is looking
 );
 
 // model matrix
 glm::mat4 MODEL;
 // projection matrix
 glm::mat4 PROJ;
-// camera transform matrix 
+// camera transform matrix
 glm::mat4 VIEW;
 
 // projection matrix parameters
 float FOV = 45.0f; // field of view
-float NEAR_CLIP = 0.01f; 
-float FAR_CLIP = 10.0f; 
+float NEAR_CLIP = 0.01f;
+float FAR_CLIP = 1000.0f;
 
 static void print_opengl_info() {
     std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "Shading: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "Shading: " << glGetString(GL_SHADING_LANGUAGE_VERSION)
+              << std::endl;
 }
 
 void initialize_program() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "Cannot initialize SDL, exiting" << std::endl;
-        exit(1);
-    }
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-                        SDL_GL_CONTEXT_PROFILE_CORE);
-
-    WINDOW = SDL_CreateWindow("mesher", SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT,
-                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    if (WINDOW == nullptr) {
-        std::cerr << "Cannot create window" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    GL_CONTEXT = SDL_GL_CreateContext(WINDOW);
-    if (GL_CONTEXT == nullptr) {
-        std::cerr << "Cannot create opengl context, exiting" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
-        std::cerr << "glad is not initialized, exiting" << std::endl;
-        exit(EXIT_FAILURE);
-    }
+    ctx.create_window(1200, 900);
     print_opengl_info();
-    SDL_SetWindowFullscreen(WINDOW, SDL_FALSE);
-
     // Load shaders
     fs::path vertex_shader_path = ROOT / "shaders/vertex_shader.glsl";
     fs::path fragment_shader_path = ROOT / "shaders/fragment_shader.glsl";
-    if(!fs::exists(vertex_shader_path) || !fs::exists(fragment_shader_path)){
+    if (!fs::exists(vertex_shader_path) || !fs::exists(fragment_shader_path)) {
         std::cerr << "Could not find shaders path" << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -101,38 +67,50 @@ void initialize_program() {
 
 void handle_input() {
     SDL_Event event;
-    static int xpos = WIDTH / 2;
-    static int ypos = HEIGHT / 2;
-    static bool clicked = false;
+    // clicked, is_left
+    static auto mouse_state = std::make_pair<bool, bool>(false, 0);
     while (SDL_PollEvent(&event) != 0) {
-        if (event.type == SDL_QUIT){
-            QUIT = true;
+        if (event.type == SDL_QUIT) {
+            ctx.is_quit = true;
             return;
-        }
-        else if (event.type == SDL_DROPFILE){
+        } else if (event.type == SDL_DROPFILE) {
             std::cout << event.drop.file << std::endl;
             mesh = Mesh(std::string(event.drop.file));
-        }
-        else if (event.type == SDL_KEYDOWN){
+        } else if (event.type == SDL_KEYDOWN) {
             camera.handle_key_action(event.key.keysym.sym, 0.05f);
-        }
-        else if (event.type == SDL_MOUSEBUTTONUP || event.type == SDL_MOUSEBUTTONDOWN)
-            clicked = (event.type ==  SDL_MOUSEBUTTONDOWN);
+        } else if (event.type == SDL_MOUSEBUTTONUP ||
+                   event.type == SDL_MOUSEBUTTONDOWN) {
+            mouse_state.first = (event.type == SDL_MOUSEBUTTONDOWN);
+            mouse_state.second = SDL_BUTTON(SDL_BUTTON_LEFT);
+        } else if (event.type == SDL_MOUSEMOTION) {
+            if (!mouse_state.first)
+                continue;
+            // if (mouse_state.second){
+            uint32_t start = SDL_GetTicks();
+            auto triangle_opt =
+                check_intersection(glm::vec2(event.motion.x, event.motion.y),
+                                   ctx.get_viewport(), mesh, VIEW, PROJ);
+            if (triangle_opt.has_value()) {
+                triangle = highlight_triangle(triangle_opt.value());
+                uint32_t end = SDL_GetTicks();
+                std::cout << "Took: " << (end - start) << "ms" << std::endl;
+            }
 
-        else if (event.type == SDL_MOUSEMOTION){
-            if (!clicked) continue;
-            xpos = event.motion.xrel;
-            ypos = event.motion.yrel;
-            camera.handle_mouse_action(xpos, ypos);
+            //     continue;
+            // }
+            // // right is clicked
+            // int xpos = event.motion.xrel;
+            // int ypos = event.motion.yrel;
+            // camera.handle_mouse_action(xpos, ypos);
         }
     }
 }
 
 void pre_draw() {
+    ctx.update_window();
 
-    SDL_GetCurrentDisplayMode(0, &DM);
-    int WIDTH = DM.w;
-    int HEIGHT = DM.h;
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
 
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_DEPTH_TEST);
@@ -141,7 +119,7 @@ void pre_draw() {
     // set the winding
     glFrontFace(GL_CCW);
 
-    glViewport(0, 0, WIDTH, HEIGHT);
+    glViewport(0, 0, ctx.width, ctx.height);
     glClearColor(0.2f, 0.2f, 0.2f, 1.f);
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     glPolygonMode(GL_FRONT, GL_LINE);
@@ -149,8 +127,9 @@ void pre_draw() {
 
     shader.use();
     MODEL = mesh.get_model_matrix();
-    PROJ  = glm::perspective(
-        glm::radians(FOV), (float) WIDTH / (float) HEIGHT, NEAR_CLIP, FAR_CLIP);
+    PROJ = glm::perspective(glm::radians(FOV),
+                            (float)ctx.width / (float)ctx.height, NEAR_CLIP,
+                            FAR_CLIP);
     VIEW = camera.get_view_matrix();
 
     glm::vec3 light_source1 = glm::vec3(0.0f, 2.0f, 3.0f);
@@ -162,31 +141,30 @@ void pre_draw() {
 
     shader.set_uniform("u_LightSource1", light_source1);
     shader.set_uniform("u_LightSource2", light_source2);
+    shader.set_uniform("u_LightSource2", light_source2);
 }
 
 void main_loop() {
-    uint64_t start, end, count = 0;
-    float elapsed;
-    while (!QUIT) {
-        start = SDL_GetPerformanceCounter();
+    // uint64_t start, end, count = 0;
+    // float elapsed;
+    // mesh_box = Mesh_bounding_box(mesh);
+    while (!ctx.is_quit) {
+        // start = SDL_GetPerformanceCounter();
         handle_input();
         pre_draw();
         mesh.draw(shader);
-        SDL_GL_SwapWindow(WINDOW);
+        triangle.draw(shader);
+        // mesh_box.draw(shader);
+        SDL_GL_SwapWindow(ctx.window);
         // print FPS every every 5 rounds
-        if (count == 5){
-            end = SDL_GetPerformanceCounter();
-            elapsed = (end-start)/ (float) SDL_GetPerformanceFrequency();
-            std::cout << "FPS: "<< std::to_string(1.0f/elapsed) << std::endl;
-            count = 0;
-        }
-        count += 1;
+        // if (count == 5){
+        //     end = SDL_GetPerformanceCounter();
+        //     elapsed = (end-start)/ (float) SDL_GetPerformanceFrequency();
+        //     // std::cout << "FPS: "<< std::to_string(1.0f/elapsed) <<
+        //     std::endl; count = 0;
+        // }
+        // count += 1;
     }
-}
-
-void cleanup() {
-    SDL_DestroyWindow(WINDOW);
-    SDL_Quit();
 }
 
 int main(int argc, char *argv[]) {
@@ -194,8 +172,9 @@ int main(int argc, char *argv[]) {
     // read files from command line
     if (argc > 1) {
         mesh = Mesh(std::string(argv[1]));
+        std::cout << mesh.triangles.size() << std::endl;
+        triangle = highlight_triangle(mesh.triangles.at(0));
     }
     main_loop();
-    cleanup();
     return 0;
 }
